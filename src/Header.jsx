8a47b81fe54bf2c2713beb46027c8796ceb311dc
hukurou <junjun.0712.on@gmail.com>
2024-05-17 16:42:18 +0900

    change style of header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { styled } from '@mui/system';
 
-export default function Header() {
+// スタイルを定義
+const StyledAppBar = styled(AppBar)({
+  backgroundColor: '#464B3C', // ヘッダーの背景色
+  borderBottom: '1px solid #B5B282', // 下線の色
+});
+
+const StyledTypography = styled(Typography)({
+  flexGrow: 1,
+  color: '#B5B282', // タイトルの色
+});
+
+const StyledButton = styled(Button)({
+  color: '#B5B282', // ナビゲーションリンクの色
+  marginLeft: '20px',
+});
+
+function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/record">Record</Link></li>
-          <li><Link to="/recruit">Recruit</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-    </header>
-  )
+    <StyledAppBar position="static">
+      <Toolbar>
+        <StyledTypography variant="h6">
+          関西学院大学 室内合奏団
+        </StyledTypography>
+        <StyledButton component={Link} to="/">
+          ホーム
+        </StyledButton>
+        <StyledButton component={Link} to="/record">
+          過去の演奏
+        </StyledButton>
+        <StyledButton component={Link} to="/recruit">
+          団員募集
+        </StyledButton>
+        <StyledButton component={Link} to="/contact">
+          お問い合わせ
+        </StyledButton>
+      </Toolbar>
+    </StyledAppBar>
+  );
 }
+
+export default Header;
