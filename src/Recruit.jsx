4ea04cc64781de3e8a51f0b41f25e6b331e9d67b
daachan <daichan2416@gmail.com>
2024-06-08 12:14:35 +0900

    component transplant 1

--- src/Recruit.jsx
+++ src/Recruit.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import { Container, Box, Typography } from '@mui/material'
 
 export default function Recruit() {
+  // ----{style}----
+
+  // Q&A
   const QA_style = {
-    padding: 2,
+    paddingTop: 2,
+    paddingBottom: 2,
     marginBottom: 2,
   };
 
@@ -12,11 +16,14 @@ export default function Recruit() {
     marginBottom: 1,
   };
 
+  // ----{Body}----
+
   return (
     <Container>
       <Typography variant='h1' gutterBottom>
         団員募集
       </Typography>
+      {/* ---- 団体紹介 ---- */}
       <Box
         sx={{
           border: '1px solid',
@@ -34,6 +41,8 @@ export default function Recruit() {
           音楽や楽器が好きな方大学で音楽をやってみようと思っている方アンサンブルやオーケストラに興味がある方私たちと一緒に新しいことを始めてみませんか？
         </Typography>
       </Box>
+
+      {/* ---- 募集パート ---- */}
       <Box
         sx={{
           border: '1px solid',
@@ -48,11 +57,11 @@ export default function Recruit() {
           募集パート
         </Typography>
         <Typography variant="body1" gutterBottom>
-          弦楽器
-          管楽器
-          打楽器
+          {/* --- ここに募集パートリストを作る --- */}
         </Typography>
       </Box>
+
+      {/* ---- 活動日・場所 ---- */}
       <Box
         sx={{
           border: '1px solid',
@@ -63,13 +72,32 @@ export default function Recruit() {
           backgroundColor: 'background.paper', // テーマの背景色を適用
         }}>
 
-        <Typography variant="h6" gutterBottom>
-          活動日・場所
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          練習時間
-        </Typography>
+        <Box>
+          <Typography variant="h6" gutterBottom>
+            活動日
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            毎週2回 17:00～20:00(授業期間中)
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            毎週2回程度 10:00～16:00(長期休暇中)
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="h6" gutterBottom>
+            場所
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            クラブハウス棟2F 205号室・ミーティングルーム
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            クラブハウスは↓の丸印の場所です！
+            6号館から、正門と逆方向に進むと正面に階段があるので、 そこを降り、右手に見える建物です。 気軽に見学・楽器体験に来てください！
+          </Typography>
+        </Box>
       </Box>
+
+      {/* ---- Q&A ---- */}
       <Box
         sx={{
           border: '1px solid',
@@ -89,7 +117,10 @@ export default function Recruit() {
             Q どんなサークル？
           </Typography>
           <Typography variant="body1" gutterBottom>
-            A 神戸三田キャンパスで活動する管弦楽サークルです。
+            A 神戸三田キャンパスで活動する管弦楽サークルです。 
+            学祭や依頼演奏会ではアンサンブル（少人数での演奏）、 
+            定期演奏会では、市民の方々やOB・OGとオーケストラとして演奏しています。 
+            音楽を楽しむことをモットーに活動しております。
           </Typography>
         </Box>
         <Box sx={QA_style}>
@@ -105,7 +136,7 @@ export default function Recruit() {
             Q 見学に行きたいです！
           </Typography>
           <Typography variant="body1" gutterBottom>
-            A いつでもどうぞ！
+            A 問い合わせから連絡もらえれば、 団員が案内します！アポなしでもOKです！
           </Typography>
         </Box>
         <Box sx={QA_style}>
@@ -114,6 +145,49 @@ export default function Recruit() {
           </Typography>
           <Typography variant="body1" gutterBottom>
             A 大歓迎です！優しい先輩が丁寧に教えます！
+            現在所属している団員の多くが 楽器未経験の状態からはじめています。
+          </Typography>
+        </Box>
+        <Box sx={QA_style}>
+          <Typography variant="body1" sx={QA_index}>
+            Q 楽器を持っていないのですが...
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            A 団持ちの楽器があり、貸出できるので、 自分の楽器がなくても大丈夫です。
+            ただ、楽器によっては団持ちのものがなかったり数が足りないことがあるので、 一度団員に聞いてみてください！
+          </Typography>
+        </Box>
+        <Box sx={QA_style}>
+          <Typography variant="body1" sx={QA_index}>
+            Q 団費はいくらですか？
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            A 月1500円です。
+          </Typography>
+        </Box>
+        <Box sx={QA_style}>
+          <Typography variant="body1" sx={QA_index}>
+            Q 5限に授業があっても大丈夫？
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            A 5限が終わってからの練習もOKです！
+          </Typography>
+        </Box>
+        <Box sx={QA_style}>
+          <Typography variant="body1" sx={QA_index}>
+            Q 学業とサークルの両立はできますか？
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            A 演奏会前は異なりますが、基本週2日練習に参加する形です。
+            試験前の活動はお休みとなるので、試験勉強に集中できます。 また、ほとんどの団員がアルバイトとサークルを両立しています^^*
+          </Typography>
+        </Box>
+        <Box sx={QA_style}>
+          <Typography variant="body1" sx={QA_index}>
+            Q 興味があるのだけど、どこに連絡したらいいですか？
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            A 関西学院室内合奏団についてのお問い合わせはこちら！
           </Typography>
         </Box>
       </Box>
